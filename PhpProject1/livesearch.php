220f761aba446eebb6ef57bf31b5dbf71eda87e8
shahbazakhtar <shahbazakhtar90@gmail.com>
2014-05-09 20:19:50 +0530

    Update livesearch.php

--- PhpProject1/livesearch.php
+++ PhpProject1/livesearch.php
@@ -4,6 +4,7 @@ $q=$_POST['str'];
 $dim=$_POST['dim'];
 $type=$_POST['type'];
 $lang=$_POST['lang'];
+$id=$_POST['id'];
 $type=strtolower($type);
 $hint="";
 if (strlen($q)>0) {
@@ -11,32 +12,41 @@ if (strlen($q)>0) {
 		{
 		if($dim==1)
 		{
-			$hint.='int* '.$name;
-			$hint.='<br>';
-			$hint.='int '.$name.'[]';
+			echo '<select name="dropdown" onchange="getData(this.value,'.$id.')">
+			      <option value="">-- Select --</option>
+                  <option value="int*">int* '.$name.' </option>
+                  <option value="int[]">int '.$name. '[]</option>
+                  </select>';
 		}
 		if($dim==2)
 		{
-			$hint.='int** '.$name;
+			echo '<select name="dropdown" onchange="getData(this.value,'.$id.')">
+			      <option value="">-- Select --</option>
+                  <option value="int**">int** '.$name.' </option>
+                  </select>';
 		}
 	    }
 	    if($lang=="Java")
 	    {
 			if($dim==1)
 		    {
-			$hint.='int[] '.$name;
-			$hint.='<br>';
-			$hint.='ArrayList '.$name;
-			$hint.='<br>';
-			$hint.='Set<Integer> '.$name;
+			echo '<select name="dropdown" onchange="getData(this.value,'.$id.')">
+			      <option value="">-- Select --</option>
+                  <option value="int[]">int[] '.$name.' </option>
+                  <option value="ArrayList">ArrayList '.$name. '</option>
+                  <option value="Set<Integer>">Set&ltInteger&gt '.$name. '</option>
+                  </select>';
 		    }
 		    if($dim==2)
 		    {
-			$hint.='int[][] '.$name;
+			echo '<select name="dropdown" onchange="getData(this.value,'.$id.')">
+			      <option value="">-- Select --</option>
+                  <option value="int[][]">int[][] '.$name.' </option>
+                  </select>';
 		    }
 		}
 		
-	} 
+} 
 if ($hint=="") {
   $response="no suggestion";
 } else {
@@ -44,5 +54,5 @@ if ($hint=="") {
 }
 
 //output the response
-echo $response;
+//echo $response;
 ?>
